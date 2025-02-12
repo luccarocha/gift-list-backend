1e6f1c7845193ca08b031b47353c5621d4306e4e
luccarocha <mr.lucca.tec@gmail.com>
2025-02-11 23:07:26 -0300

    Fix port

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const jsonServer = require('json-server');
 const cors = require('cors');
+const path = require('path');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(cors());
 server.use(middlewares);
 server.use(router);
 
-const port = process.env.PORT || 3000;
+const port = 10000; // Porta que o Render está detectando
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
