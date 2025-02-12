69e9590982848275282e306751a89e94e23075af
luccarocha <mr.lucca.tec@gmail.com>
2025-02-11 23:33:11 -0300

    Fix server variables

--- server.js
+++ server.js
@@ -8,40 +8,40 @@ const port = process.env.PORT || 10000;
 app.use(cors());
 app.use(express.json());
 
-// Crie os dados iniciais se não existirem
-const defaultData = {
- "gifts": [
-   {"id": 1, "name": "Assadeiras", "link": "https://www.magazinevoce.com.br/magazineluccastorels/jogo-4-formas-assadeiras-retangulares-aluminio-p-bolos-e-tortas-postagem-rapida-mba/p/hhfghh5j8e/ud/fabo/"},
-   {"id": 2, "name": "Batedeira", "link": "https://www.magazinevoce.com.br/magazineluccastorels/batedeira-britania-diamante-inox-turbo-550w-preta/p/ag97b59j57/ep/btdc/"},
-   {"id": 3, "name": "Cafeteira", "link": "https://www.magazinevoce.com.br/magazineluccastorels/cafeteira-eletrica-electrolux-efficient-ecm10-15-cafes-preto-e-granite-gray/p/231236700/ep/otep/"},
-   {"id": 4, "name": "Chaleira Elétrica", "link": "https://www.magazinevoce.com.br/magazineluccastorels/jarra-eletrica-bule-chaleira-inox-18-litros-amanzi-fix-110v-preta-inox-prime/p/cdb6jhc22a/ep/clre/"},
-   {"id": 5, "name": "Cortina Escritório", "link": "https://produto.mercadolivre.com.br/MLB-3629565701-cortina-tecido-blackout-100-com-voil-flame-gaze-300x230-_JM?attributes=COLOR_SECONDARY_COLOR:Q2luemE="},
-   {"id": 6, "name": "Cortina Quarto", "link": "https://produto.mercadolivre.com.br/MLB-3629565701-cortina-tecido-blackout-100-com-voil-flame-gaze-300x230-_JM?attributes=COLOR_SECONDARY_COLOR:Q2luemE="},
-   {"id": 7, "name": "Ferro de Passar", "link": "https://www.magazinevoce.com.br/magazineluccastorels/ferro-de-passar-roupa-a-vapor-e-a-seco-electrolux-efficient-esi10-azul/p/235627500/ep/ferr/"},
-   {"id": 8, "name": "Jogo de Faca", "link": "https://www.magazinevoce.com.br/magazineluccastorels/jogo-de-facas-tramontina-plenus-4-pecas-inox/p/agjh0e56h5/ud/fcud/"},
-   {"id": 9, "name": "Jogo de Jantar", "link": "https://www.magazinevoce.com.br/magazineluccastorels/aparelho-de-jantar-e-cha-20-pecas-biona-de-ceramica-redondo-branco-e-azul-donna/p/143282300/ud/apja/"},
-   {"id": 10, "name": "Jogo de Panelas", "link": "https://www.magazinevoce.com.br/magazineluccastorels/jogo-de-panelas-tramontina-turim-em-aluminio-com-revestimento-interno-e-externo-em-antiaderente-starflon-max-preto-10-pecas/p/ajgg173kk8/ud/cjpn/"},
-   {"id": 11, "name": "Jogo de Talheres Inox", "link": "https://www.magazinevoce.com.br/magazineluccastorels/jogo-de-talheres-tramontina-inox-colher-faca-garfo-24-pecas-inox/p/cf3jkk5gh8/ud/faqu"},
-   {"id": 12, "name": "Jogo de Talheres Preto", "link": "https://www.magazinevoce.com.br/magazineluccastorels/jogo-de-faqueiro-tramontina-talheres-24-pecas-ipanema-cores/p/jk8fe29ajb/ud/faqu/"},
-   {"id": 13, "name": "Kit 6 Taças", "link": "https://www.magazinevoce.com.br/magazineluccastorels/kit-6-tacas-vidro-para-vinho-agua-385ml-nadir-figueiredo/p/142245300/ud/tavi/"},
-   {"id": 14, "name": "Kit 6 Taças Champanhe", "link": "https://www.magazinevoce.com.br/magazineluccastorels/jogo-taca-champanhe-espumante-186ml-buffet-6-pecas-nadir/p/kf1gf1kfke/af/tads/"},
-   {"id": 15, "name": "Kit 6 Taças de Sobremesa", "link": "https://www.magazinevoce.com.br/magazineluccastorels/jogo-de-tacas-de-sobremesa-de-vidro-160ml-6-pecas-haus-concept-gelato/p/237309900/ud/tcsb/"},
-   {"id": 16, "name": "Kit Cobre Leito", "link": "https://www.mercadolivre.com.br/kit-cobre-leito-percal-200-fios-casal-unique-diversas-cores-cor-grafite/p/MLB27826443?attributes=COLOR:Grafite"},
-   {"id": 17, "name": "Kit Utensílios De Cozinha", "link": "https://www.magazineluiza.com.br/jogo-kit-com-12-pecas-utensilios-de-cozinha-colheres-espatulas-pegador-silicone-copo-suporte-prime/p/de7d9kc0ca/ud/cjtu/"},
-   {"id": 18, "name": "Liquidificador", "link": "https://www.magazinevoce.com.br/magazineluccastorels/liquidificador-mondial-easy-power-l-550-w-preto-2-velocidades-550w/p/021723000/ep/lqac/"},
-   {"id": 19, "name": "Lixeira Banheiro 1", "link": "https://www.magazinevoce.com.br/magazineluccastorels/lixeira-cesto-lixo-banheiro-5-litros-cozinha-escritorio-preta-viel/p/dg02he79a1/ud/udli/"},
-   {"id": 20, "name": "Lixeira Banheiro 2", "link": "https://www.magazinevoce.com.br/magazineluccastorels/lixeira-cesto-lixo-banheiro-5-litros-cozinha-escritorio-preta-viel/p/dg02he79a1/ud/udli/"},
-   {"id": 21, "name": "Lixeira Cozinha", "link": "https://www.magazinevoce.com.br/magazineluccastorels/lixeira-cesto-lixo-banheiro-5-litros-cozinha-escritorio-preta-viel/p/dg02he79a1/ud/udli/"},
-   {"id": 22, "name": "Mop", "link": "https://www.magazinevoce.com.br/magazineluccastorels/mop-giratorio-novica-original-com-balde/p/226737000/ud/mopi/"},
-   {"id": 23, "name": "Panela de Pressão", "link": "https://www.magazinevoce.com.br/magazineluccastorels/panela-de-pressao-aluminio-45l-fechamento-externo-grafite-preto-maximum-panelux/p/aceb0ac141/ud/udpp/"},
-   {"id": 24, "name": "Potes de Vidro Hermético", "link": "https://www.magazinevoce.com.br/magazineluccastorels/conjunto-5-potes-de-vidro-hermetico-mantimentos-tampa-640ml-u4home/p/gk6k1dcdjk/ud/porm/"},
-   {"id": 25, "name": "Sanduicheira", "link": "https://www.magazinevoce.com.br/magazineluccastorels/sanduicheira-grill-britania-bgr27i-2-em-1-prata-850w-antiaderente/p/235076200/ep/gset/"},
-   {"id": 26, "name": "Varal Retrátil", "link": "https://www.magazinevoce.com.br/magazineluccastorels/varal-de-chao-com-abas-retratil-roupas-intimas-apartamento-articulado-mini-aco-branco-portatil-preto-home-utilities/p/cd7aab54k9/ud/vral/"}
- ],
- "selectedGifts": []
+// Dados dos presentes
+const giftData = {
+  gifts: [
+    {"id": 1, "name": "Assadeiras", "link": "https://www.magazinevoce.com.br/magazineluccastorels/jogo-4-formas-assadeiras-retangulares-aluminio-p-bolos-e-tortas-postagem-rapida-mba/p/hhfghh5j8e/ud/fabo/"},
+    {"id": 2, "name": "Batedeira", "link": "https://www.magazinevoce.com.br/magazineluccastorels/batedeira-britania-diamante-inox-turbo-550w-preta/p/ag97b59j57/ep/btdc/"},
+    {"id": 3, "name": "Cafeteira", "link": "https://www.magazinevoce.com.br/magazineluccastorels/cafeteira-eletrica-electrolux-efficient-ecm10-15-cafes-preto-e-granite-gray/p/231236700/ep/otep/"},
+    {"id": 4, "name": "Chaleira Elétrica", "link": "https://www.magazinevoce.com.br/magazineluccastorels/jarra-eletrica-bule-chaleira-inox-18-litros-amanzi-fix-110v-preta-inox-prime/p/cdb6jhc22a/ep/clre/"},
+    {"id": 5, "name": "Cortina Escritório", "link": "https://produto.mercadolivre.com.br/MLB-3629565701-cortina-tecido-blackout-100-com-voil-flame-gaze-300x230-_JM?attributes=COLOR_SECONDARY_COLOR:Q2luemE="},
+    {"id": 6, "name": "Cortina Quarto", "link": "https://produto.mercadolivre.com.br/MLB-3629565701-cortina-tecido-blackout-100-com-voil-flame-gaze-300x230-_JM?attributes=COLOR_SECONDARY_COLOR:Q2luemE="},
+    {"id": 7, "name": "Ferro de Passar", "link": "https://www.magazinevoce.com.br/magazineluccastorels/ferro-de-passar-roupa-a-vapor-e-a-seco-electrolux-efficient-esi10-azul/p/235627500/ep/ferr/"},
+    {"id": 8, "name": "Jogo de Faca", "link": "https://www.magazinevoce.com.br/magazineluccastorels/jogo-de-facas-tramontina-plenus-4-pecas-inox/p/agjh0e56h5/ud/fcud/"},
+    {"id": 9, "name": "Jogo de Jantar", "link": "https://www.magazinevoce.com.br/magazineluccastorels/aparelho-de-jantar-e-cha-20-pecas-biona-de-ceramica-redondo-branco-e-azul-donna/p/143282300/ud/apja/"},
+    {"id": 10, "name": "Jogo de Panelas", "link": "https://www.magazinevoce.com.br/magazineluccastorels/jogo-de-panelas-tramontina-turim-em-aluminio-com-revestimento-interno-e-externo-em-antiaderente-starflon-max-preto-10-pecas/p/ajgg173kk8/ud/cjpn/"},
+    {"id": 11, "name": "Jogo de Talheres Inox", "link": "https://www.magazinevoce.com.br/magazineluccastorels/jogo-de-talheres-tramontina-inox-colher-faca-garfo-24-pecas-inox/p/cf3jkk5gh8/ud/faqu"},
+    {"id": 12, "name": "Jogo de Talheres Preto", "link": "https://www.magazinevoce.com.br/magazineluccastorels/jogo-de-faqueiro-tramontina-talheres-24-pecas-ipanema-cores/p/jk8fe29ajb/ud/faqu/"},
+    {"id": 13, "name": "Kit 6 Taças", "link": "https://www.magazinevoce.com.br/magazineluccastorels/kit-6-tacas-vidro-para-vinho-agua-385ml-nadir-figueiredo/p/142245300/ud/tavi/"},
+    {"id": 14, "name": "Kit 6 Taças Champanhe", "link": "https://www.magazinevoce.com.br/magazineluccastorels/jogo-taca-champanhe-espumante-186ml-buffet-6-pecas-nadir/p/kf1gf1kfke/af/tads/"},
+    {"id": 15, "name": "Kit 6 Taças de Sobremesa", "link": "https://www.magazinevoce.com.br/magazineluccastorels/jogo-de-tacas-de-sobremesa-de-vidro-160ml-6-pecas-haus-concept-gelato/p/237309900/ud/tcsb/"},
+    {"id": 16, "name": "Kit Cobre Leito", "link": "https://www.mercadolivre.com.br/kit-cobre-leito-percal-200-fios-casal-unique-diversas-cores-cor-grafite/p/MLB27826443?attributes=COLOR:Grafite"},
+    {"id": 17, "name": "Kit Utensílios De Cozinha", "link": "https://www.magazineluiza.com.br/jogo-kit-com-12-pecas-utensilios-de-cozinha-colheres-espatulas-pegador-silicone-copo-suporte-prime/p/de7d9kc0ca/ud/cjtu/"},
+    {"id": 18, "name": "Liquidificador", "link": "https://www.magazinevoce.com.br/magazineluccastorels/liquidificador-mondial-easy-power-l-550-w-preto-2-velocidades-550w/p/021723000/ep/lqac/"},
+    {"id": 19, "name": "Lixeira Banheiro 1", "link": "https://www.magazinevoce.com.br/magazineluccastorels/lixeira-cesto-lixo-banheiro-5-litros-cozinha-escritorio-preta-viel/p/dg02he79a1/ud/udli/"},
+    {"id": 20, "name": "Lixeira Banheiro 2", "link": "https://www.magazinevoce.com.br/magazineluccastorels/lixeira-cesto-lixo-banheiro-5-litros-cozinha-escritorio-preta-viel/p/dg02he79a1/ud/udli/"},
+    {"id": 21, "name": "Lixeira Cozinha", "link": "https://www.magazinevoce.com.br/magazineluccastorels/lixeira-cesto-lixo-banheiro-5-litros-cozinha-escritorio-preta-viel/p/dg02he79a1/ud/udli/"},
+    {"id": 22, "name": "Mop", "link": "https://www.magazinevoce.com.br/magazineluccastorels/mop-giratorio-novica-original-com-balde/p/226737000/ud/mopi/"},
+    {"id": 23, "name": "Panela de Pressão", "link": "https://www.magazinevoce.com.br/magazineluccastorels/panela-de-pressao-aluminio-45l-fechamento-externo-grafite-preto-maximum-panelux/p/aceb0ac141/ud/udpp/"},
+    {"id": 24, "name": "Potes de Vidro Hermético", "link": "https://www.magazinevoce.com.br/magazineluccastorels/conjunto-5-potes-de-vidro-hermetico-mantimentos-tampa-640ml-u4home/p/gk6k1dcdjk/ud/porm/"},
+    {"id": 25, "name": "Sanduicheira", "link": "https://www.magazinevoce.com.br/magazineluccastorels/sanduicheira-grill-britania-bgr27i-2-em-1-prata-850w-antiaderente/p/235076200/ep/gset/"},
+    {"id": 26, "name": "Varal Retrátil", "link": "https://www.magazinevoce.com.br/magazineluccastorels/varal-de-chao-com-abas-retratil-roupas-intimas-apartamento-articulado-mini-aco-branco-portatil-preto-home-utilities/p/cd7aab54k9/ud/vral/"}
+  ],
+  selectedGifts: []
 };
 
-let selectedGifts = [];
+let { gifts, selectedGifts } = giftData;
 
 // Rotas
 app.get('/gifts', (req, res) => {
@@ -69,4 +69,6 @@ app.post('/returnGift', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
+    console.log(`Available gifts: ${gifts.length}`);
+    console.log(`Selected gifts: ${selectedGifts.length}`);
 });
